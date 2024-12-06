a32f21fc6e0ecc18a20ee18ff3dfd36ce0338cce
KaichenQu <qu.ka@northeastern.edu>
2024-12-05 18:55:58 -0800

    3 finish

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -59,3 +59,11 @@ export const findUserById = async (id: string) => {
   const response = await axios.get(`${USERS_API}/${id}`);
   return response.data;
 };
+export const deleteUser = async (userId: string) => {
+  const response = await axiosWithCredentials.delete(`${USERS_API}/${userId}`);
+  return response.data;
+};
+export const createUser = async (user: any) => {
+  const response = await axios.post(`${USERS_API}`, user);
+  return response.data;
+};
